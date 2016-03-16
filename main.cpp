f78b26d3ca1183fc6a21406aa3a3c00ce809d976
karols <karols>
2016-03-16 14:31:40 +0100

    Adding obstacle class

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
-
-//
-// Disclamer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resource, use the helper
-// method resourcePath() from ResourcePath.hpp
-//
-
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include "Obstacle.h"
 
 // Here is a small helper for you ! Have a look.
 #include "ResourcePath.hpp"
@@ -23,7 +8,10 @@
 int main(int, char const**)
 {
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::RenderWindow window(sf::VideoMode(1200, 680), "CS 1.5 alpha");
+
+	//Create obstacle
+	Obstacle* obstacle1 = new Obstacle(10,10,30,50);
 
     // Start the game loop
     while (window.isOpen())
@@ -32,6 +20,7 @@ int main(int, char const**)
         sf::Event event;
         while (window.pollEvent(event))
         {
+
             // Close window: exit
             if (event.type == sf::Event::Closed) {
                 window.close();
@@ -45,6 +34,9 @@ int main(int, char const**)
 
         // Clear screen
         window.clear();
+
+		//Draw obstacle
+		obstacle1->draw(window);
 
         // Update the window
         window.display();
